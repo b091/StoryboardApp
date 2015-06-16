843396124f75585bffebd9c5ee27b8280e66c144
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-06-16 14:14:43 +0200

    added more deesc

--- app/storyboard/services/StorieService.js
+++ app/storyboard/services/StorieService.js
@@ -5,8 +5,8 @@ angular.module('StoryboardApp.Common').service('StoryService', function() {
     service.stories = [
         {
             id: 1,
-            title: 'First story',
-            description: 'Some description',
+            title: 'If you shine or fly with a shining chaos, surrender invents you.',
+            description: 'Protons walk from peaces like carnivorous ships. When the lotus of love develops peace of the doer, the reincarnation will know sinner.',
             status: 'Todo',
             type: 'Spike',
             assignee: 1,
@@ -15,10 +15,20 @@ angular.module('StoryboardApp.Common').service('StoryService', function() {
         },
         {
             id: 2,
-            title: 'Second one',
-            description: 'Some long description ......... .....',
+            title: 'Why does the ship tremble?',
+            description: 'Sun of a fine malaria, fire the adventure! Calm voyages lead to the peace. Jolly roger, yo-ho-ho.',
             status: 'In Progress',
-            type: 'Enchantment',
+            type: 'Bug',
+            assignee: 1,
+            criteria: 'AC: adsd asd asdasd',
+            reporter: 2
+        },
+        {
+            id: 3,
+            title: 'For an iced ripe platter, add some vinegar and corn syrup.',
+            description: 'Sun of a fine malaria, fire the adventure! Calm voyages lead to the peace. Jolly roger, yo-ho-ho.',
+            status: 'In Progress',
+            type: 'Enhancement',
             assignee: 1,
             criteria: 'AC: adsd asd asdasd',
             reporter: 2
