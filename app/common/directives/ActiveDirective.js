3c11793c88caf2cdb8f58b78e29ccffd6518c01e
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-06-17 10:23:52 +0200

    fade

---
+++ app/common/directives/ActiveDirective.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('StoryboardApp.Common')
+    .directive('active', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                element.mouseenter(function() {
+                    element.finish().fadeTo('slow', 0.7);
+                }).mouseleave(function() {
+                    element.finish().fadeTo('slow', 1);
+                });
+            }
+        }
+    });
