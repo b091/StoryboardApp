8c7962c077c4a4ea423618b82bb244c68e218f28
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-06-18 10:50:14 +0200

    reformated

--- app/users/services/UserService.js
+++ app/users/services/UserService.js
@@ -2,23 +2,23 @@
 
 angular.module('StoryboardApp.Users')
     .service('UserService', ['EndpointService', '$http', function(EndpointService, $http) {
-    var service = this,
-        moduleName = 'users';
+        var service = this,
+            moduleName = 'users';
 
-    service.model = [
-        {
-            id:1,
-            firstName: 'Gutless',
-            lastName: 'Ecce',
-            email: 'gutless@ecce.com'
-        },
-        {
-            id:2,
-            firstName: 'Valebats',
-            lastName: 'Caesium',
-            email: 'valebats@caesium.com'
-        }
-    ];
+        service.model = [
+            {
+                id: 1,
+                firstName: 'Gutless',
+                lastName: 'Ecce',
+                email: 'gutless@ecce.com'
+            },
+            {
+                id: 2,
+                firstName: 'Valebats',
+                lastName: 'Caesium',
+                email: 'valebats@caesium.com'
+            }
+        ];
 
 
-}]);
+    }]);
